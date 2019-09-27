cfccd84927cb209248cef3dd71c7cc50aa9b16a6
Davy Gueudré <davy.gueudre@acatus.fr>
2019-09-27 17:15:09 +0200

    Report date d'inscriptions

--- src/UcaBundle/Resources/sql/Reprise/ReportDebutInscription.sql
+++ src/UcaBundle/Resources/sql/Reprise/ReportDebutInscription.sql
@@ -1,2 +1,2 @@
-UPDATE format_activite SET date_debut_inscription = '2019-09-30 09:00:00'
-WHERE date_debut_inscription < '2019-09-30 09:00:00';
+UPDATE format_activite SET date_debut_inscription = '2019-09-30 14:00:00'
+WHERE date_debut_inscription < '2019-09-30 14:00:00';
